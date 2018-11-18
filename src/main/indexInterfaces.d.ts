0da699eed45247acb31e463ed41041687a69efc7
UMR-Haozi <244075473@qq.com>
2018-11-18 13:10:22 +0800

    IndexMain

---
+++ src/main/indexInterfaces.d.ts
@@ -0,0 +1,10 @@
+// Defines the interface of the structure of a task
+interface IMainVoteInfo {
+    id:number,
+    title:string
+}
+
+interface IRegisterState {
+    confirmDirty:boolean,
+    autoCompleteResult:any
+}
