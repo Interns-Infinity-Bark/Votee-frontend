d333b22788a7de42bd24fd33a504c55daa73a4de
bLue <tbdblue@gmail.com>
2018-11-29 02:53:08 +0800

    Just help these guys, again and again and again...

--- src/main/IndexShowVoteTitle.jsx
+++ src/main/IndexShowVoteTitle.jsx
@@ -1,39 +1,18 @@
-
 import * as React from "react";
 import {List} from "antd";
-// 测试用数据，真实情况CheckUrl直接请求数据
-let testList = [
-    {id:1,title:"我帅不帅"},
-    {id:2,title:"blue帅不帅"},
-    {id:3,title:"你纳爷今天能不能吃鸡"},
-    {id:4,title:"如何才能变得像blue一样强"}];
+
 export class IndexShowVoteTitle extends React.Component{
     constructor(props){
         super(props);
-        // 这里只是测试用
-        this.state = {voteInfoList:[]};
     }
-    // 挂载时调用
-    componentDidMount(){
-        this.getData();
-        console.log('hhhhh');
-    }
-    // 数据更新时调用
-    componentDidUpdate(){
-        // this.getData()
-    }
-    // 获取数据
-    getData(){
-        // fetch(this.props.CheckUrl,(err,data))
-        this.setState({voteInfoList:testList});
-    }
+
     render() {
         return(
                 <List
                     // header={<div>Header</div>}
                     // footer={<div>Footer</div>}
                     bordered
-                    dataSource={this.state.voteInfoList}
+                    dataSource={this.props.votes}
                     renderItem={function (item) {
                         return <List.Item>{item.title}</List.Item>;
                     }}
--- src/main/ShowAllVoteInfo.jsx
+++ src/main/ShowAllVoteInfo.jsx
@@ -2,8 +2,29 @@ import * as React from "react";
 import {IndexShowVoteTitle} from "./IndexShowVoteTitle";
 import {Row,Col} from "antd";
 import './Index.css'
+import { get } from '../utils/request';
+import { api } from '../configs';
 
 export class ShowAllVoteInfo extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            ongoingVotes: [],
+            endedVotes: [],
+        };
+    }
+
+    async componentDidMount() {
+        const ongoingVotesData = await get(`${api.base}/ongoingVotes`);
+        ongoingVotesData.status === 'ok' && this.setState({
+            ongoingVotes: ongoingVotesData.data.votes,
+        });
+        const endedVotesData = await get(`${api.base}/endedVotes`);
+        endedVotesData.status === 'ok' && this.setState({
+            endedVotes: endedVotesData.data.votes,
+        });
+    }
+
     render() {
         return (
             <div>
@@ -11,13 +32,13 @@ export class ShowAllVoteInfo extends React.Component{
                     <Col span={6} offset={4}>
                         <div>
                             <h2 style={{ marginBottom: 26,marginTop:16 }}>正在进行的投票</h2>
-                            <IndexShowVoteTitle CheckUrl="" />
+                            <IndexShowVoteTitle CheckUrl="" votes={this.state.ongoingVotes} />
                         </div>
                     </Col>
                     <Col span={6} offset={4}>
                         <div>
                             <h2 style={{ marginBottom: 26,marginTop:16  }}>已经结束的投票</h2>
-                            <IndexShowVoteTitle CheckUrl="" />
+                            <IndexShowVoteTitle CheckUrl="" votes={this.state.endedVotes} />
                         </div>
                     </Col>
                 </Row>
